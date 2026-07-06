9a7322c9508e7ea24f77475d88f96300c4495ea3
Wolkendo Arias <99550549+maestrowoldo@users.noreply.github.com>
2026-07-06 15:29:36 -0300

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import process from "node:process";
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://wolkendoarias.com";
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://wolkendo.dev";
 
   return [
     {
